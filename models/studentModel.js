346de19344ebf3b963a12bc19d8393d4c2971a1d
Abderrahman Lahiaouni <lahiaouni.abd@gmail.com>
2023-04-30 14:33:49 +0200

    removed id from models to use default one

--- models/studentModel.js
+++ models/studentModel.js
@@ -1,16 +1,46 @@
 const mongoose = require('mongoose');
 
 const studentSchema = new mongoose.Schema({
-  id: { type: Number, required: true },
-  fullName: { type: String, required: true },
-  personalIdNumber: { type: String, required: true },
-  phoneNumber: { type: String, required: true },
-  email: { type: String, required: true },
-  personalAddress: { type: String, required: true },
-  hasPaidFees: { type: Boolean, required: true },
-  personalIdCopy: { type: Buffer, required: true },
-  degree: { type: Buffer, required: true },
-  remainingFees: { type: Number, required: true },
+  firstName: {
+    type: String,
+    required: true,
+  },
+  lastName: {
+    type: String,
+    required: true,
+  },
+  personalIdNumber: {
+    type: String,
+    required: true,
+  },
+  phoneNumber: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  address: {
+    type: String,
+    required: true,
+  },
+  isPaid: {
+    type: Boolean,
+    required: true,
+  },
+  personalIdCopy: {
+    type: String,
+    required: true,
+  },
+  degree: {
+    type: String,
+    required: true,
+  },
+  remainingFees: {
+    type: Number,
+    required: true,
+  },
 });
 
 const Student = mongoose.model('Student', studentSchema);
--- models/teachingStaffModel.js
+++ models/teachingStaffModel.js
@@ -1,17 +1,50 @@
 const mongoose = require('mongoose');
 
 const teachingStaffSchema = new mongoose.Schema({
-  id: { type: Number, required: true },
-  fullName: { type: String, required: true },
-  personalIdNumber: { type: String, required: true },
-  phoneNumber: { type: String, required: true },
-  email: { type: String, required: true },
-  personalAddress: { type: String, required: true },
-  subjectTaught: { type: String, required: true },
-  salary: { type: Number, required: true },
-  isPaid: { type: Boolean, required: true },
-  personalIdCopy: { type: Buffer, required: true },
-  resume: { type: Buffer, required: true },
+  firstName: {
+    type: String,
+    required: true,
+  },
+  lastName: {
+    type: String,
+    required: true,
+  },
+  personalIdNumber: {
+    type: String,
+    required: true,
+  },
+  phoneNumber: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  address: {
+    type: String,
+    required: true,
+  },
+  subject: {
+    type: String,
+    required: true,
+  },
+  salary: {
+    type: Number,
+    required: true,
+  },
+  isPaid: {
+    type: Boolean,
+    required: true,
+  },
+  personalIdCopy: {
+    type: String,
+    required: true,
+  },
+  resume: {
+    type: String,
+    required: true,
+  },
 });
 
 const TeachingStaff = mongoose.model('TeachingStaff', teachingStaffSchema);
